e691d0ea7dc0e04e7b534f865a6f1212cdd2c03f
Administrator <liu2272772@163.com>
2017-08-18 21:46:25 +0800

    刷新

--- www/js/baseInfo.js
+++ www/js/baseInfo.js
@@ -1,5 +1,5 @@
 angular.module("app.baseInfo", [])
-  .controller("BaseInfoController", function ($scope, webService, commonFactory) {
+  .controller("BaseInfoController", function ($scope, $timeout, webService, commonFactory) {
 
     //获取我的基本信息
     baseInfo();
@@ -11,5 +11,16 @@ angular.module("app.baseInfo", [])
       commonFactory.loadingShow();
       $scope.myBaseInfo = webService.getBaseInfo();
       commonFactory.loadingHide();
+    }
+
+    /**
+     * 刷新
+     */
+    $scope.doRefresh = function () {
+      $scope.myBaseInfo = webService.getBaseInfo();
+      $timeout(function () {
+        //停止广播ion-refresher
+        $scope.$broadcast('scroll.refreshComplete');
+      }, 2000);
     }
   });
--- www/js/skill.js
+++ www/js/skill.js
@@ -2,7 +2,7 @@
  * Created by Administrator on 2017/8/16 0016.
  */
 angular.module("app.skill", [])
-  .controller("SkillController", function ($scope, webService, commonFactory) {
+  .controller("SkillController", function ($scope, $timeout, webService, commonFactory) {
 
     //获取我的基本信息
     skillInfo();
@@ -14,5 +14,15 @@ angular.module("app.skill", [])
       commonFactory.loadingShow();
       $scope.skillInfos = webService.getSkillInfo();
       commonFactory.loadingHide();
+    }
+    /**
+     * 刷新
+     */
+    $scope.doRefresh = function () {
+      $scope.myBaseInfo = webService.getBaseInfo();
+      $timeout(function () {
+        //停止广播ion-refresher
+        $scope.$broadcast('scroll.refreshComplete');
+      }, 2000);
     }
   });
