c0f1a66e363b1543a2318bff18879961fce0241b
Administrator <liu2272772@163.com>
2017-08-20 20:10:12 +0800

    项目信息模态框

--- www/js/appProject.js
+++ www/js/appProject.js
@@ -2,7 +2,16 @@
  * Created by Administrator on 2017/8/19 0019.
  */
 angular.module("app.appProject", [])
-  .controller("appProjectController", function ($scope, $timeout, webService, commonFactory) {
+  .controller("appProjectController", function ($scope, $timeout, webService, commonFactory, $ionicModal, $ionicSlideBoxDelegate) {
+
+    /**
+     * 模态框
+     */
+    $ionicModal.fromTemplateUrl('templates/appProjectModal.html', {
+      scope: $scope
+    }).then(function(modal) {
+      $scope.modal = modal;
+    });
 
     //获取项目信息
     getProjectInfo();
@@ -41,7 +50,34 @@ angular.module("app.appProject", [])
     /**
      * 向上拖拽
      */
+    var onDragUpTime = 0;
     $scope.onDragUpSlide = function () {
 
+      if (onDragUpTime == 30) {
+        onDragUpTime++;
+        $scope.modal.show();
+      } else if (onDragUpTime < 31){
+        onDragUpTime++;
+      }
+    }
+
+
+
+    /**
+     * 关闭模态框
+     */
+    $scope.closeProjectModal = function () {
+      onDragUpTime = 0;
+      $scope.modal.hide();
+    }
+
+    /**
+     * 跳转到对应项目
+     * @param index
+     */
+    $scope.goProject = function (index) {
+      onDragUpTime = 0;
+      $ionicSlideBoxDelegate.slide(index, 1000);
+      $scope.modal.hide();
     }
   });
